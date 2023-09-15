8d5074ecfe35122aa7eadeb50c602295cfab616b
wujie1993 <qq594jj@gmail.com>
2023-09-15 09:48:16 +0800

    fix logger
    Signed-off-by: wujie1993 <qq594jj@gmail.com>

--- logger.go
+++ logger.go
@@ -3,22 +3,22 @@ package daemon
 import "log"
 
 type Logger interface {
-	Trace(format string)
+	Trace(args ...interface{})
 	Tracef(format string, args ...interface{})
 
-	Debug(format string)
+	Debug(args ...interface{})
 	Debugf(format string, args ...interface{})
 
-	Info(format string)
+	Info(args ...interface{})
 	Infof(format string, args ...interface{})
 
-	Warn(format string)
+	Warn(args ...interface{})
 	Warnf(format string, args ...interface{})
 
-	Error(format string)
+	Error(args ...interface{})
 	Errorf(format string, args ...interface{})
 
-	Fatal(format string)
+	Fatal(args ...interface{})
 	Fatalf(format string, args ...interface{})
 }
 
@@ -28,46 +28,46 @@ func (l DefaultLog) Tracef(format string, args ...interface{}) {
 	log.Printf("[trace] "+format, args...)
 }
 
-func (l DefaultLog) Trace(format string) {
-	l.Tracef(format)
+func (l DefaultLog) Trace(args ...interface{}) {
+	log.Print(append([]interface{}{"[trace]"}, args...))
 }
 
 func (l DefaultLog) Debugf(format string, args ...interface{}) {
 	log.Printf("[debug] "+format, args...)
 }
 
-func (l DefaultLog) Debug(format string) {
-	l.Debugf(format)
+func (l DefaultLog) Debug(args ...interface{}) {
+	log.Print(append([]interface{}{"[debug]"}, args...))
 }
 
 func (l DefaultLog) Infof(format string, args ...interface{}) {
 	log.Printf("[info] "+format, args...)
 }
 
-func (l DefaultLog) Info(format string) {
-	l.Infof(format)
+func (l DefaultLog) Info(args ...interface{}) {
+	log.Print(append([]interface{}{"[info]"}, args...))
 }
 
 func (l DefaultLog) Warnf(format string, args ...interface{}) {
 	log.Printf("[warn] "+format, args...)
 }
 
-func (l DefaultLog) Warn(format string) {
-	l.Warnf(format)
+func (l DefaultLog) Warn(args ...interface{}) {
+	log.Print(append([]interface{}{"[warn]"}, args...))
 }
 
 func (l DefaultLog) Errorf(format string, args ...interface{}) {
 	log.Printf("[error] "+format, args...)
 }
 
-func (l DefaultLog) Error(format string) {
-	l.Errorf(format)
+func (l DefaultLog) Error(args ...interface{}) {
+	log.Print(append([]interface{}{"[error]"}, args...))
 }
 
 func (l DefaultLog) Fatalf(format string, args ...interface{}) {
 	log.Printf("[fatal] "+format, args...)
 }
 
-func (l DefaultLog) Fatal(format string) {
-	l.Fatalf(format)
+func (l DefaultLog) Fatal(args ...interface{}) {
+	log.Print(append([]interface{}{"[fatal]"}, args...))
 }
